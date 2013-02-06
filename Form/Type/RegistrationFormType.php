771d2f5bd1ef5731bb837d98d9d9c76bcf201109
Lal Mohan <lal@amt.in>
2013-02-06 16:39:31 +0530

    translation updated

--- Form/Type/RegistrationFormType.php
+++ Form/Type/RegistrationFormType.php
@@ -31,13 +31,13 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username', null, array('label' => 'Username', 'translation_domain' => 'FOSUserBundle'))
-            ->add('email', 'email', array('label' => 'Email', 'translation_domain' => 'FOSUserBundle'))
+            ->add('username', null, array('label' => 'form.username', 'translation_domain' => 'FOSUserBundle'))
+            ->add('email', 'email', array('label' => 'form.email', 'translation_domain' => 'FOSUserBundle'))
             ->add('plainPassword', 'repeated', array(
                 'type' => 'password',
                 'options' => array('translation_domain' => 'FOSUserBundle'),
-                'first_options' => array('label' => 'Password'),
-                'second_options' => array('label' => 'Confirm password'),
+                'first_options' => array('label' => 'form.password'),
+                'second_options' => array('label' => 'form.password_confirmation'),
             ))
         ;
     }
